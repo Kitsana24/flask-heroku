2d2e6e5b492f823afb54d6f3cfa2045b21c5d410
Kitsana24 <87840503+Kitsana24@users.noreply.github.com>
2021-07-30 09:54:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,9 @@ def hello():
 def get_api():
     return jsonify(data)
 
+@app.route('/name')
+def name():
+    return"กฤษณา อยู่ถนอม เลขที่5 ชั้นม.4/10"    
+
 if __name__ == "__main__":
     app.run(debug=False)
