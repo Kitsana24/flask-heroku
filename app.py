3ef7329f5e89694121bb71c67bb3f6363204e10f
Kitsana24 <87840503+Kitsana24@users.noreply.github.com>
2021-08-20 09:30:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def get_api():
 
 @app.route('/name')
 def name():
-    return"กฤษณา อยู่ถนอม<br> เลขที่5 ชั้นม.4/10"    
+    return"<font <color=purple></font>กฤษณา อยู่ถนอม<br> เลขที่5 ชั้นม.4/10"    
 
 if __name__ == "__main__":
     app.run(debug=False)
